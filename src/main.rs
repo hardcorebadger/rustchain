b566e1b381adcac583fbf903059bf352908d64c2
Jake <jake.trefethen@gmail.com>
2018-08-11 14:21:22 -0700

    webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate iron;
+#[macro_use] extern crate mime;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
 mod blockchain;
 mod block;
 mod transaction;
@@ -5,7 +13,21 @@ mod transaction;
 use blockchain::Blockchain;
 
 fn main() {
-    println!("Hello, world!");
-    let mut chain = Blockchain::new();
-    chain.new_transaction("me","you",5);
+	let mut chain = Blockchain::new();
+  println!("Serving on http://localhost:3000...");
+  
+  let mut router = Router::new();
+
+  router.get("/", get, "root");
+  Iron::new(router).http("localhost:3000").unwrap();
+    // chain.new_transaction("me","you",5);
+}
+
+fn get(_request: &mut Request) -> IronResult<Response> {
+	let mut response = Response::new();
+	response.set_mut(status::Ok);
+	response.set_mut(mime!(Text/Html; Charset=Utf8));
+	response.set_mut("hey!");
+
+	Ok(response)
 }
